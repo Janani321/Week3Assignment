602ab766aeae0ce307e0ad54cdb705966546839a
Janani321 <144647195+Janani321@users.noreply.github.com>
2023-09-26 16:40:00 +0530

    Add files via upload

---
+++ ChangeOddIndexToUppercase.java
@@ -0,0 +1,18 @@
+package week3.day2;
+
+public class ChangeOddIndexToUppercase {
+
+	public static void main(String[] args) {
+
+// TODO Auto-generated method stub
+		String test = "changeme";
+char[] charArray = test.toCharArray();
+        
+        for (int i = 1; i < charArray.length; i += 2) {
+            charArray[i] = Character.toUpperCase(charArray[i]);
+        }
+        String result = new String(charArray);
+        System.out.println(result);
+	}
+
+}
